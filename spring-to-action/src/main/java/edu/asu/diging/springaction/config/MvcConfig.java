c4c4743fd6e2a56f9da916f022ea5e53d30ce625
Julia Damerow <julia.damerow@gmail.com>
2022-02-16 13:09:22 -0500

    initial commit

---
+++ spring-to-action/src/main/java/edu/asu/diging/springaction/config/MvcConfig.java
@@ -0,0 +1,9 @@
+package edu.asu.diging.springaction.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("edu.asu.diging.simpleusers.web")
+public class MvcConfig {
+
+}
